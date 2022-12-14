35878ab13a6c359e82087ad8ff0f1c3a3c1e9732
SUMIT KUMAR <110215880+mrsumit2002@users.noreply.github.com>
2022-12-15 01:04:30 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 let add = document.querySelector("#add");
 let subtract = document.querySelector("#subtract");
+let reset = document.querySelector("#reset");
 
 add.addEventListener("click", function () {
   let output = document.querySelector("#output");
@@ -13,4 +14,10 @@ subtract.addEventListener("click", function () {
     let result1 = Number(output.innerText) - 1;
   
     output.innerText = result1;
+  });
+
+  reset.addEventListener("click", function () {
+    document.querySelector("#output").innerText = 0;
+    
+
   });
